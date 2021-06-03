8c040e432c440f50aaa7b9a3bc0feecd63788f2c
Rmiverson <Rmiverson99@gmail.com>
2021-06-03 13:52:07 -0700

    setup for auth and user in controllers

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,10 +1,3 @@
-class ProjectsController < ApplicationController
-   def create
-      user = User.create(user_params)
+class Api::V1::ProjectsController < ApplicationController
 
-      if user.valid?
-         token = encode_token({user_id: user.id})
-         render json: {user: user, token: token}
-      end
-   end
 end
--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,2 +1,17 @@
-class UserController < ApplicationController
+class Api::V1::UserController < ApplicationController
+   def create
+      user = User.create(user_params)
+
+      if user.valid?
+         token = encode_token({user_id: user.id})
+         render json: {user: user, token: token}
+      else
+         render json: {error: "Invalid username or password"}
+      end
+   end
+
+   def show
+      user = User.find(params[:id])
+      render json: UserSerializer.new(user).serialized_json
+   end
 end
